b875a6d7f241fd03efdf4a294ae7265eccce4cc2
mr <1501615455@qq.com>
2022-01-13 00:33:01 +0800

    接入http

--- spring-learn/src/main/java/com/example/spring/learn/http/HttpInvokeClient.java
+++ spring-learn/src/main/java/com/example/spring/learn/http/HttpInvokeClient.java
@@ -1,16 +1,27 @@
 package com.example.spring.learn.http;
 
+import com.google.common.base.Charsets;
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -26,28 +37,88 @@ public class HttpInvokeClient {
 
     private static final CloseableHttpClient HTTP_CLIENT = HttpClientBuilder.create()
             .setDefaultRequestConfig(RequestConfig.custom()
-            .setConnectTimeout(5000).setSocketTimeout(10000).setConnectionRequestTimeout(10000).build())
+                    .setConnectTimeout(5000)
+                    .setSocketTimeout(10000)
+                    .setConnectionRequestTimeout(10000)
+                    .build())
             .setConnectionTimeToLive(15, TimeUnit.MINUTES)
             .disableCookieManagement()
             .setMaxConnTotal(1000)
             .setMaxConnPerRoute(50)
             .build();
 
-    public static String get(String url, Integer timeoutMs) throws Exception {
+    public static String get(String url, Integer timeoutMs) {
         HttpGet method = new HttpGet(url);
         if (timeoutMs != null) {
             method.setConfig(buildTimeOutConfig(timeoutMs));
         }
-        final CloseableHttpResponse response = HTTP_CLIENT.execute(method);
-        final String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-            return responseBody;
-        } else {
-            logger.error("http get error, url:{}, returnCode:{}, response:{}", url, response.getStatusLine(), responseBody);
-            return null;
-
+        try {
+            final CloseableHttpResponse response = HTTP_CLIENT.execute(method);
+            final String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                logger.info("HttpInvokeClient get execute success, url:{}, result:{}", url, responseBody);
+                return responseBody;
+            } else {
+                logger.info("HttpInvokeClient get failed, url:{}, returnCode:{}, response:{}", url, response.getStatusLine(), responseBody);
+            }
+        } catch (Exception e) {
+            logger.error("HttpInvokeClient get error, url:" + url, e);
         }
+        return null;
+    }
 
+    public static String post(String url, Map<String, String> paramMap, Integer timeoutMs) {
+        HttpPost method = new HttpPost(url);
+        if (timeoutMs != null) {
+            method.setConfig(buildTimeOutConfig(timeoutMs));
+        }
+        if (paramMap != null && !paramMap.isEmpty()) {
+            List<NameValuePair> formItem = Lists.newArrayList();
+            for(String key : paramMap.keySet()) {
+                if (paramMap.get(key) != null) {
+                    formItem.add(new BasicNameValuePair(key, paramMap.get(key)));
+                }
+            }
+            final UrlEncodedFormEntity form = new UrlEncodedFormEntity(formItem, Charsets.UTF_8);
+            method.setEntity(form);
+        }
+        try {
+            final CloseableHttpResponse response = HTTP_CLIENT.execute(method);
+            final String responseBody = EntityUtils.toString(response.getEntity(), Charsets.UTF_8);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                logger.info("HttpInvokeClient post success, url:{}, result:{}", url, response);
+                return responseBody;
+            } else {
+                logger.info("HttpInvokeClient post failed, url:{}, param:{}, resultCode:{}, response:{}", url, paramMap, response.getStatusLine().getStatusCode(), responseBody);
+            }
+        } catch (Exception e) {
+            logger.error("HttpInvokeClient post error, url:" + url, e);
+        }
+        return null;
+    }
+
+    public static String postWithJsonParam(String url, String jsonParam, Integer timeoutMs) {
+        HttpPost method = new HttpPost(url);
+        if (timeoutMs != null) {
+            method.setConfig(buildTimeOutConfig(timeoutMs));
+        }
+        if (StringUtils.isNotBlank(jsonParam)) {
+            method.setHeader("Content-Type", "application/json; charset=UTF-8");
+            method.setEntity(new StringEntity(jsonParam, ContentType.APPLICATION_JSON));
+        }
+        try {
+            final CloseableHttpResponse response = HTTP_CLIENT.execute(method);
+            final String responseBody = EntityUtils.toString(response.getEntity(), Charsets.UTF_8);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                logger.info("HttpInvokeClient post success, url:{}, result:{}", url, response);
+                return responseBody;
+            } else {
+                logger.info("HttpInvokeClient post failed, url:{}, param:{}, resultCode:{}, response:{}", url, jsonParam, response.getStatusLine().getStatusCode(), responseBody);
+            }
+        } catch (Exception e) {
+            logger.error("HttpInvokeClient post error, url:" + url, e);
+        }
+        return null;
     }
 
     private static RequestConfig buildTimeOutConfig(int timeout){
@@ -65,23 +136,21 @@ public class HttpInvokeClient {
                 }
             });
 
-    public static void main(String[] args) throws Exception {
-
-        for (int i = 0; i < 2; i++) {
-            final String url = "http://www.baidu.com";
-            FIXED_THREAD_POOL.execute(() -> {
-                String res = null;
-                try {
-                    res = HttpInvokeClient.get(url, 5000);
-                } catch (Exception e){
-
-                }
-                if (res != null) {
-                    System.out.println(url);
-                    System.out.println(res);
-                }
-            });
-
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//        for (int i = 0; i < 100; i++) {
+//            FIXED_THREAD_POOL.execute(() -> {
+//                String res = null;
+//                try {
+//                    res = HttpInvokeClient.get(url, 5000);
+//                } catch (Exception e){
+//
+//                }
+//                if (res != null) {
+//                    System.out.println(url);
+//                    System.out.println(res);
+//                }
+//            });
+//
+//        }
+//    }
 }
--- spring-learn/src/main/java/com/example/spring/learn/springmvc/controller/TestController.java
+++ spring-learn/src/main/java/com/example/spring/learn/springmvc/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.example.spring.learn.springmvc.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.example.spring.learn.http.HttpInvokeClient;
 import com.example.spring.learn.mq.eventbus.EventBusPublish;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @Author
@@ -27,5 +31,20 @@ public class TestController {
     public String eventBus(String message) {
         eventBusPublish.publishMessage(message);
         return "ok";
+    }
+
+    @GetMapping("http/get")
+    public String getHttpTest(String url, Integer timeoutMs) {
+        return HttpInvokeClient.get(url, timeoutMs);
+    }
+
+    @GetMapping("http/post")
+    public String postHttpTest(String url, Integer timeoutMs, String type, String jsonParam) {
+        if (type.equals("json")) {
+            return HttpInvokeClient.postWithJsonParam(url, jsonParam, timeoutMs);
+        } else {
+            Map<String, String> map = JSON.parseObject(jsonParam, Map.class);
+            return HttpInvokeClient.post(url, map, timeoutMs);
+        }
     }
 }
